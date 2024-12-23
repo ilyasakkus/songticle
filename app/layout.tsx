ea2b6506bdc4f0222d036ccf2c325a91a7181748
İlyas Akkuş <ilyasbote@gmail.com>
2024-12-23 19:23:23 +0300

    ssd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import { Navbar } from '@/components/Navbar'
 import { Sidebar } from '@/components/Sidebar'
 import './globals.css'
-import { SupabaseProvider } from './providers/SupabaseProvider'
-import { Toaster } from 'react-hot-toast'
+import { Inter } from 'next/font/google'
 import { ThemeProvider } from './components/ThemeProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Songticle',
   description: 'Share your favorite songs with stories',
 }
@@ -21,25 +18,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      </head>
       <body className={inter.className}>
-        <SupabaseProvider>
-          <Toaster />
+        <ThemeProvider>
           <div className="flex h-screen">
             <Sidebar />
             <div className="flex-1 flex flex-col">
-              <ThemeProvider>
-                <Navbar />
-                <main className="flex-1 overflow-y-auto">
-                  {children}
-                </main>
-              </ThemeProvider>
+              <Navbar />
+              <main className="flex-1 overflow-y-auto">
+                {children}
+              </main>
             </div>
           </div>
-        </SupabaseProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
