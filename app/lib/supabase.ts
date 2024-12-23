c46373724c05f2ca7bc890b5184f1ac152bce4a0
İlyas Akkuş <ilyasbote@gmail.com>
2024-12-23 17:17:38 +0300

    sd

---
+++ app/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://wxyzxkgugqfofypuzeth.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4eXp4a2d1Z3Fmb2Z5cHV6ZXRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3MDY3NTIsImV4cCI6MjA0ODI4Mjc1Mn0.eJZrUmujxj-FWj1MxmdLXod1Fse0b_atvr7MWryTW4Q'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
