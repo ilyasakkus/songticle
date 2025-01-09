b245b523c827218383cb832d9c572e4a238f4778
İlyas Akkuş <ilyasbote@gmail.com>
2025-01-09 17:45:08 +0300

    sd

--- app/album/[id]/page.tsx
+++ app/album/[id]/page.tsx
@@ -1,9 +1,33 @@
+import { GetServerSideProps } from 'next'
+
 type Props = {
   params: {
     id: string
   }
 }
 
-export default function Page(props: Props) {
-  return null
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+  const { id } = context.params as { id: string }
+
+  // Perform any data fetching or processing here
+
+  return {
+    props: {
+      params: {
+        id
+      }
+    }
+  }
+}
+
+export default function Page({ params }: Props) {
+  const { id } = params
+
+  // Render your component using the id
+  return (
+    <div>
+      <h1>Album ID: {id}</h1>
+      {/* Add more content here */}
+    </div>
+  )
 } 
