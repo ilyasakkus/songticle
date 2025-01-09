1820c2fa1cebb25b6711aedc805ccf1b78e147db
İlyas Akkuş <ilyasbote@gmail.com>
2025-01-09 17:07:06 +0300

    sd

--- app/album/[id]/page.tsx
+++ app/album/[id]/page.tsx
@@ -1,5 +1,17 @@
+import { Metadata } from 'next'
 import AlbumContent from './AlbumContent'
 
-export default async function AlbumPage({ params }: { params: { id: string } }) {
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export default function AlbumPage({ params }: Props) {
   return <AlbumContent id={params.id} />
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  return {
+    title: `Album - ${params.id}`,
+  }
 } 
