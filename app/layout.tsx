7fe310bc3933dd4f2aaa47a9cc039f2d0483af65
İlyas Akkuş <ilyasbote@gmail.com>
2025-01-10 22:38:11 +0300

    dsf

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
 import { AuthProvider } from './providers/AuthProvider'
+import { ThemeProvider } from "@/components/providers/theme-provider"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,19 +19,26 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" data-theme="dark" suppressHydrationWarning>
-      <body className={inter.className} suppressHydrationWarning>
-        <AuthProvider>
-          <div className="min-h-screen flex flex-col">
-            <Header />
-            <div className="flex-1 flex">
-              <Sidebar />
-              <main className="flex-1 bg-base-200 p-4">
-                {children}
-              </main>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AuthProvider>
+            <div className="min-h-screen flex flex-col">
+              <Header />
+              <div className="flex-1 flex">
+                <Sidebar />
+                <main className="flex-1 bg-base-200 p-4">
+                  {children}
+                </main>
+              </div>
             </div>
-          </div>
-        </AuthProvider>
+          </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
