845cb53bf9e2857a388613284bdb0660883a478e
İlyas Akkuş <ilyasbote@gmail.com>
2025-01-10 22:39:50 +0300

    sd

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
 import { AuthProvider } from './providers/AuthProvider'
-import { ThemeProvider } from "@/components/providers/theme-provider"
+import { ThemeProvider } from './components/providers/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
