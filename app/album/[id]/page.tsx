fe8bca5968510f93acc1e9790898da627e1d3f1b
İlyas Akkuş <ilyasbote@gmail.com>
2025-01-10 15:24:12 +0300

    ss

--- app/album/[id]/page.tsx
+++ app/album/[id]/page.tsx
@@ -1,22 +1,15 @@
-import { Suspense } from 'react'
+import { Suspense, use } from 'react'
 import AlbumClient from './components/AlbumClient'
 
-type Props = {
-  params: Promise<{
-    id: string
-  }>
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}
-
-export default async function AlbumPage({ params }: Props) {
-  const resolvedParams = await params
+export default function AlbumPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = use(params)
   return (
     <Suspense fallback={
       <div className="flex justify-center items-center min-h-[200px]">
         <span className="loading loading-spinner loading-lg"></span>
       </div>
     }>
-      <AlbumClient albumId={resolvedParams.id} />
+      <AlbumClient albumId={id} />
     </Suspense>
   )
 } 
