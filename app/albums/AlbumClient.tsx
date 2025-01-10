d2c1d5bd469879eeba6737bf4bb539bfe04d3d46
İlyas Akkuş <ilyasbote@gmail.com>
2025-01-10 17:18:35 +0300

    sdsd

--- app/albums/AlbumClient.tsx
+++ app/albums/AlbumClient.tsx
@@ -1,8 +1,23 @@
 import Link from 'next/link'
 
+interface Album {
+  artists: {
+    id: string
+    name: string
+  }
+}
+
+interface Props {
+  album: Album
+}
+
+const AlbumClient = ({ album }: Props) => (
   <Link 
     href={`/artists/${album.artists.id}`}
     className="text-xl text-primary hover:underline mb-2 inline-block"
   >
     {album.artists.name}
   </Link> 
+)
+
+export default AlbumClient 
